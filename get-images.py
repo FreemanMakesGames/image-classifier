e6e630e2f9d9b39feb00f69be909bf2d6a73b5e5
Freeman <freeman@freemanmakesgames.pro>
2020-09-28 14:00:37 -0700

    Screenshots are captured from a video with a 20-second delay each time in an automatic and fast manner.

---
+++ get-images.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+gap_sec = 20  # Delay in seconds between screenshots.
+
+vid_cap = cv.VideoCapture( "raw-data/first-person-shooter/csgo/csgo.mp4" )
+
+gap_frames = gap_sec * vid_cap.get( cv.CAP_PROP_FPS )
+
+counter = 0
+while vid_cap.isOpened():
+
+    vid_cap.set( cv.CAP_PROP_POS_FRAMES, gap_frames * counter )
+
+    ret, frame = vid_cap.read()
+
+    if not ret:
+        break
+
+    counter += 1
+
+    cv.imwrite( "processed-data/{0}.jpg".format( counter ), frame )
+
+vid_cap.release()
+cv.destroyAllWindows()
+
