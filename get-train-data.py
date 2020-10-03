e0b3fadf6372acf14199d04894afd52e80d25fc3
Freeman <freeman@freemanmakesgames.pro>
2020-10-03 12:06:00 -0700

    Syntax of using the built-in log reg model finally checks out..

--- get-train-data.py
+++ get-train-data.py
@@ -2,6 +2,7 @@ import os
 import sys
 import cv2 as cv
 import numpy as np
+from pprint import pprint
 
 processed_data_dir = "/home/insight/Documents/Projects/image-classifier/processed-data"
 
@@ -22,13 +23,16 @@ model.setTrainMethod( cv.ml.LOGISTIC_REGRESSION_BATCH )
 images = []
 labels = []
 
-for genre_entry in os.scandir( processed_data_dir ):
+index_to_genre = {}
+
+for i, genre_entry in enumerate( os.scandir( processed_data_dir ) ):
 
     if not genre_entry.is_dir():
         print( "Warning: A non-dir under processed data dir?" )
         continue
 
-    label = genre_entry.name
+    genre_name = genre_entry.name
+    index_to_genre[ i ] = genre_name
 
     for game_entry in os.scandir( genre_entry.path ):
 
@@ -49,12 +53,24 @@ for genre_entry in os.scandir( processed_data_dir ):
             img = np.float32( img.flatten() / 255 )
 
             images.append( img )
-            labels.append( 0 )
+            labels.append( i )
 
 # Create train data object.
 images = np.array( images )
-labels = np.transpose( np.array( labels ) )  # Labels has to be a column vector.
-data = cv.ml.TrainData_create( images, cv.ml.ROW_SAMPLE, labels )
+labels = np.float32( np.array( labels ) )
+#data = cv.ml.TrainData_create( images, cv.ml.ROW_SAMPLE, labels )
 
-print( data )
+pprint( labels )
+
+model.train( images, cv.ml.ROW_SAMPLE, labels )
+
+pprint( "Thetas: ", model.get_learnt_thetas() )
+
+test_img = np.float32( cv.imread( "/home/insight/Documents/Projects/image-classifier/processed-data/third-person-shooter/dead-space/dead-space-0.mp4_0.jpg", cv.IMREAD_GRAYSCALE ) )
+test_img = test_img.flatten() / 255
+test_img = test_img.reshape( 1, -1 )
+
+retval, results = model.predict( test_img )
+print( retval )
+print( results )
 
