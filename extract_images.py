345aa7569002d045c481ff2d3d69d5aa4a7a05d0
Freeman <freeman@freemanmakesgames.pro>
2020-10-13 03:43:21 -0700

    Fix and extracting images works.

--- extract_images.py
+++ extract_images.py
@@ -1,15 +1,18 @@
+import os
 import sys
 import argparse
 
 from images_extraction import get_images_from_game_dir
 
+# Parse args.
 argparser = argparse.ArgumentParser( description = "Extract images from videos." )
 argparser.add_argument( "--raw-data-dir", "-rdd" )
 argparser.add_argument( "--save-dir", "-sd" )
+argparser.add_argument( "--img-per-game", "-ipg", type = int )
 args = argparser.parse_args()
-
 raw_data_dir = args.raw_data_dir
 save_dir = args.save_dir
+img_per_game = args.img_per_game
 
 # Iterate all genre directories.
 for genre_entry in os.scandir( raw_data_dir ):
@@ -22,5 +25,5 @@ for genre_entry in os.scandir( raw_data_dir ):
         if not game_entry.is_dir():
             continue
 
-        get_images_from_game_dir( game_entry.path, img_per_game )
+        get_images_from_game_dir( game_entry.path, game_entry.path.replace( raw_data_dir, save_dir, 1 ), img_per_game )
 
